bd0d2b5aa6424891f522a027c4354331d11fa409
philipm92 <philip.mahlberg@hotmail.com>
2021-06-23 12:02:27 +0200

    challenge 1

---
+++ Challenges/challenge1.js
@@ -0,0 +1,24 @@
+var arr_str = [
+    ["Vienna"], 
+    [" ", "is"], 
+    [" ", " ", "a"], 
+    [" ", " ", " ", "nice"], 
+    [" ", " ", " ", " ", "city"]
+];
+
+document.write(arr_str[0][0] + arr_str[1][0] + arr_str[1][1] + arr_str[2][0]  + arr_str[2][2] + arr_str[3][0] + arr_str[3][3] + arr_str[4][0] + arr_str[4][4]);
+
+
+
+/*var arr_str = [
+    ["Vienna"], 
+    ["is"], 
+    ["a"], 
+    ["nice"], 
+    ["city"]
+];
+
+for (let i = 0; i < arr_str.length; i++) {
+    let word = arr_str[i];
+    document.write("a".repeat(i) + word + "<br>");
+}*/

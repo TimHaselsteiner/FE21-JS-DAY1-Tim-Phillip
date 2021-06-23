0f4d06ba08db43e2bbdb3cd1c0330be3619231e0
philipm92 <philip.mahlberg@hotmail.com>
2021-06-23 11:34:48 +0200

    Advanced 2

---
+++ Advanced Exercises/advanced2.js
@@ -0,0 +1,7 @@
+var mul_array = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+
+var new_arr = [mul_array[1][1], mul_array[4][2],mul_array[5][3],mul_array[2][3],mul_array[2][1]]
+/*console.log(
+    mul_array[1][1] + "," + mul_array[4][2] + "," + mul_array[5][3] + "," + mul_array[2][3] + " and " + mul_array[2][1]
+);*/
+console.log(new_arr.join());

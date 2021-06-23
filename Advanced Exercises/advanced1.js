664a7cea707a35b6b4dbc36fcd5433cc5b35c9f8
Tim  Haselsteiner <haselsteiner.t00@gmail.com>
2021-06-23 11:26:01 +0200

    Advanced1

---
+++ Advanced Exercises/advanced1.js
@@ -0,0 +1,23 @@
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+var sum1 =  a + b + Number(c) + Number(d) + e ;
+document.write(sum1+"<br>");
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+var pro1 = Number(f) * g * h * Number(i);
+document.write(pro1+"<br>");
+
+document.write(sum1/pro1);
+
+
+
+
+

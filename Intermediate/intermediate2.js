8bd4fbfb8db0891839c73f2a6145b25a3575238b
Tim  Haselsteiner <haselsteiner.t00@gmail.com>
2021-06-23 11:11:48 +0200

    intermediate 2

---
+++ Intermediate/intermediate2.js
@@ -0,0 +1,14 @@
+var fruits = ["Apple", "Banana", "Kiwi"];
+
+fruits.push("Orange");
+document.write(fruits+"<br>");
+
+
+document.write(fruits.slice(0,3)+"<br>");
+
+var animals = ["Monkey", "Horse", "Dog"];
+animals.sort();
+document.write(animals+"<br>");
+
+animals.unshift("Cat");
+document.write(animals);
